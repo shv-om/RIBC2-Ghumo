abf9d2428cc7e069641454ac3676b8a618f2da48
shv-om <shivamchaudhary6197@gmail.com>
2022-06-09 15:00:24 +0530

    District init

--- Ghumo/market/models.py
+++ Ghumo/market/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 from registration.models import Seller, Artist
 
+
+class District(models.Model):
+    district_name = models.CharField(max_length=50)
+    district_code = models.IntegerField()
+
+    def __str__(self):
+        return self.district_name
+
+
 class Marketplace(models.Model):
     item_name = models.CharField(max_length=100)
     item_price = models.IntegerField()
+    item_description = models.CharField(max_length=500)
     item_photo = models.ImageField(upload_to='all_items')
+    district_instance = models.ForeignKey(District, on_delete=models.CASCADE)
     seller_instance = models.ForeignKey(Seller, on_delete=models.CASCADE)
 
     def __str__(self):
