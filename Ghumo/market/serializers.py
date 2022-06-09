532e2ce0b8d919da282ac89134687eae49f2d881
shv-om <shivamchaudhary6197@gmail.com>
2022-06-09 12:38:29 +0530

    Init

--- Ghumo/market/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-
-class MarketplaceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Marketplace
-        fields = '__all__'
-
--- Ghumo/market/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app = "market"
-
-urlpatterns = [
-        path('marketplace', views.marketplaceview.as_view(), name="marketplace"),
-        path('event-details', views.eventdetailsview.as_view(), name="event-details"),
-]
